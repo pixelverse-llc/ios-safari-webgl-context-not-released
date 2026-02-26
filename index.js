6ba7310e8bf7e87f6a1142652647b19cda88e67b
Alex Suzuki <alex@pixelverse.ch>
2026-02-26 14:09:34 +0100

    simplify test

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
 const message = document.getElementById('message');
 
-async function run(numIterations, useOffscreenCanvas) {
 
-    for (let i = 1; i <= numIterations; i++) {
-        const canvas = useOffscreenCanvas ?
-            new OffscreenCanvas(2048, 2048) : document.createElement('canvas');
-        if (canvas instanceof HTMLCanvasElement) {
-            canvas.width = 2048;
-            canvas.height = 2048;
-        }
+async function getAndLoseContext() {
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            const canvas = new OffscreenCanvas(2048, 2048);
+            const gl = canvas.getContext('webgl2');
+            if (!gl || gl.isContextLost()) {
+                throw Error('getContext returned null or lost context');
+            }
 
-        const gl = canvas.getContext('webgl2');
-        if (!gl) {
-            throw Error('getContext returned null');
-        }
-        if (gl.isContextLost()) {
-            throw Error('getContext returned lost context');
-        }
+            // WebGL spec: "This is the recommended mechanism for applications to programmatically halt their use of the WebGL API."
+            const loseContextExtension = gl.getExtension('WEBGL_lose_context');
+            if (loseContextExtension) {
+                loseContextExtension.loseContext();
+            }
 
-        const loseContextExtension = gl.getExtension('WEBGL_lose_context');
-        if (loseContextExtension) {
-            // see: https://registry.khronos.org/webgl/extensions/WEBGL_lose_context/
-            // Implementations should destroy the underlying graphics context and all graphics resources when this
-            // method is called. This is the recommended mechanism for applications to programmatically halt their
-            // use of the WebGL API.
-            loseContextExtension.loseContext();
-            console.log(`Called loseContext() in iteration ${i}`);
-        }
-
-    }
+            resolve();
+        });
+    });
 }
 
 const startButton = document.getElementById('start');
 startButton.addEventListener('click', async () => {
     startButton.disabled = true;
-
-    message.innerText = 'Running...';
-    const offscreenCanvasCheckbox = document.getElementById('offscreen-canvas');
     const iterationsInput = document.getElementById('iterations');
     try {
-        await run(parseInt(iterationsInput.value), offscreenCanvasCheckbox.checked);
+        const numIterations = parseInt(iterationsInput.value);
+        for (let i = 1; i <= numIterations; i++) {
+            await getAndLoseContext();
+            const percentDone = (i/numIterations) * 100;
+            requestAnimationFrame(() => {
+                message.innerText = `${percentDone.toFixed(0)}% completed`;
+            });
+        }
     } catch (e) {
         message.innerText = e;
     }
