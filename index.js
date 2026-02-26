e3ca37ab5a8537c086dad4bfddf8d3ae78052505
Alex Suzuki <alex@pixelverse.ch>
2026-02-26 12:01:12 +0100

    set normal canvas size to 2048

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ async function run(numIterations, useOffscreenCanvas) {
 
     for (let i = 1; i <= numIterations; i++) {
         const canvas = useOffscreenCanvas ?
-            new OffscreenCanvas(1024, 1024) : document.createElement('canvas');
+            new OffscreenCanvas(2048, 2048) : document.createElement('canvas');
+        if (canvas instanceof HTMLCanvasElement) {
+            canvas.width = 2048;
+            canvas.height = 2048;
+        }
+
         const gl = canvas.getContext('webgl2');
         if (!gl) {
             throw Error('getContext returned null');
